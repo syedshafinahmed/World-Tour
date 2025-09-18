60e264ed8514c385bd6240687e4014327e5db58e
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-19 02:44:35 +0600

    added more style

--- src/assets/Country/Country.jsx
+++ src/assets/Country/Country.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
+    const [visited, setVisited] = useState(false);
     const handleVisited = () =>{
-        console.log("clicked");
+        // if (visited){
+        //     setVisited(false);
+        // }
+        // else{
+        //     setVisited(true);
+        // }
+
+        setVisited(!visited);
+        
+
+        // setVisited(visited ? false : true);
     }
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt="" />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
             <p>Area: {country.area.area} {country.area.area > 300000 ? "(Large Country)" : "(Small Country)"}</p>
-            <button onClick={handleVisited}>Not Visited</button>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
     );
 };
