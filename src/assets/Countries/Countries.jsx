fe618b46d0e4d36c3a44304c8f26316232ffbaba
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-19 02:25:14 +0600

    added style

--- src/assets/Countries/Countries.jsx
+++ src/assets/Countries/Countries.jsx
@@ -1,15 +1,19 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
-const Countries = ({countriesPromise}) => {
+import './Countries.css';
+const Countries = ({ countriesPromise }) => {
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
-    console.log(countries);
+    // console.log(countries);
     return (
         <div>
             <h1>Total Countries: {countries.length}</h1>
-            {
-                countries.map(country => <Country country={country}></Country>)
-            }
+            <div className='countries'>
+                {
+                    countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+                }
+            </div>
+
         </div>
     );
 };
--- src/assets/Country/Country.jsx
+++ src/assets/Country/Country.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import './Country.css'
 const Country = ({country}) => {
+    const handleVisited = () =>{
+        console.log("clicked");
+    }
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt="" />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? "(Large Country)" : "(Small Country)"}</p>
+            <button onClick={handleVisited}>Not Visited</button>
         </div>
     );
 };
